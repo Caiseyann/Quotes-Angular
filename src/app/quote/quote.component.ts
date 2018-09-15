e1c2892f4d6033566f872c4209da67fda8fe7dbb
zecollokaris <collo.kariss@gmail.com>
2018-09-15 21:32:44 +0300

    added some quotes in quote.component.ts

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,6 +8,15 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
 
+    Quotes = [
+      new Quote(1, 'Watch Finding Nemoss'),
+      new Quote(2, 'Buy Cookiesss'),
+      new Quote(3, 'Get new Phone Casess'),
+      new Quote(4, 'Get Dog Foodss'),
+      new Quote(5, 'Solve math homeworkss'),
+      new Quote(6, 'Plot my world domination planss'),
+    ]
+
 
   constructor() { }
 
