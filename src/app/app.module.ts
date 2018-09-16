41dac2283a48a16aeb6f4afc09e60618ab0e2d27
zecollokaris <collo.kariss@gmail.com>
2018-09-17 00:09:58 +0300

    added form component for quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { GoalFormComponent } from './goal-form/goal-form.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
     QuoteDetailsComponent,
     StrikethroughDirective,
     DateCountPipe,
-    GoalFormComponent
+    GoalFormComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule
