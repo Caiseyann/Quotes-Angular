6719065bf19ac2b47c09248ab90778cb2d5bea41
zecollokaris <collo.kariss@gmail.com>
2018-09-16 19:12:22 +0300

    added quotes dates tht would be displayd with quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
 
     Quotes = [
-      new Quote(1, 'Watch Finding Nemoss','Find an online version and watch merlin find his son'),
-      new Quote(2, 'Buy Cookiesss','I have to buy cookies for the parrot'),
-      new Quote(3, 'Get new Phone Casess','Diana has her birthday coming up soon'),
-      new Quote(4, 'Get Dog Foodss','Pupper likes expensive sancks'),
-      new Quote(5, 'Solve math homeworkss','Damn Math'),
-      new Quote(6, 'Plot my world domination planss','Cause I am an evil overlord'),
+      new Quote(1, 'Watch Finding Nemoss', 'Find an online version and watch merlin find his son', new Date(2018, 3, 14) ),
+      new Quote(2, 'Buy Cookiesss', 'I have to buy cookies for the parrot', new Date(2018, 6, 9) ),
+      new Quote(3, 'Get new Phone Casess', 'Diana has her birthday coming up soon', new Date(2018, 1, 12) ),
+      new Quote(4, 'Get Dog Foodss', 'Pupper likes expensive sancks', new Date(2018, 0, 18) ),
+      new Quote(5, 'Solve math homeworkss', 'Damn Math', new Date(2018, 2, 14) ),
+      new Quote(6, 'Plot my world domination planss', 'Cause I am an evil overlord', new Date(2018, 3, 14) ),
     ]
 
     toogleDetails(index){
