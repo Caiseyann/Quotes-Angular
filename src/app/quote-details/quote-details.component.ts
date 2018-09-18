23292a532c0cd37d17b19c2a5e9fb75feaf928eb
zecollokaris <collo.kariss@gmail.com>
2018-09-18 14:26:54 +0300

    added like and dislike caplability

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -16,7 +16,17 @@ export class QuoteDetailsComponent implements OnInit {
   }
 
 
-  
+downvote=0;
+upvote=0
+
+voteDown(){
+  this.downvote=this.downvote+1;
+}
+
+voteUp(){
+  this.upvote=this.upvote+1;
+}
+
 
   constructor() { }
 
